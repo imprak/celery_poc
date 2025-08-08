ef9667c1656d2cb6015aca849e57e98aff0f3563
Prakash <prakash.ct@prodapt.com>
2025-08-08 18:36:17 +0530

    switch to rabbitmq

--- celery_app.py
+++ celery_app.py
@@ -3,17 +3,25 @@ import time
 from celery import Celery
 from logger import log
 
-
-REDIS_HOST = "10.168.171.198"
-REDIS_PORT = "8083"
-REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-
-
-celery_app = Celery("celery_app", broker=REDIS_URL, backend=REDIS_URL)
+RMQ_USER = "user"
+RMQ_PASSWORD = "password"
+RMQ_HOST = "10.168.171.198"
+RMQ_PORT = "8083"
+REDIS_URL = f"amqp://{RMQ_USER}:{RMQ_PASSWORD}@{RMQ_HOST}:{RMQ_PORT}//"
 
 
-@celery_app.task(acks_late=True, reject_on_worker_lost=True)
-def continue_task(name: str) -> None:
+celery_app = Celery("celery_app", broker=REDIS_URL, backend="rpc://")
+
+
+@celery_app.task(
+    bind=True,
+    acks_late=True,
+    reject_on_worker_lost=True,
+    max_retries=3,
+    default_retry_delay=5,
+    # autoretry_for=(Exception,),
+)
+def continue_task(self, name: str) -> None:
     for i in range(10):
         log.info(f"Hello {name}, the time is {time.time()}")
         time.sleep(2)
--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,6 @@
 # Start Celery worker
 echo "Starting Celery..."
-celery -A celery_app worker --loglevel=info &
+celery -A celery_app worker --loglevel=info --pool=solo &
 
 echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn main:app --host 0.0.0.0 --port 8000
